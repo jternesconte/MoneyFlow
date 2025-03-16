634e80f5e647daffd100b039ed10ab8355bacfab
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-16 18:41:20 -0300

    feat: adds Category entity

---
+++ back/src/entities/Category.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("categories")
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  name: string;
+
+  @Column({ type: "text", nullable: false })
+  description: string;
+
+  @Column({ name: "created_at", type: "timestamp", default: () => "CURRENT_TIMESTAMP",nullable: false })
+  createdAt: string;
+
+  @Column({ name: "updated_at", type: "timestamp", nullable: true })
+  updatedAt: string;
+}
