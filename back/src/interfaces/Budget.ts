5288b176f0c19244c7949b485dfba641ca52295d
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-17 06:41:40 -0300

    feat: adds budget interface

---
+++ back/src/interfaces/Budget.ts
@@ -0,0 +1,13 @@
+import { ICategory } from "./ICategory";
+import { IUser } from "./IUser";
+
+export interface IBudget {
+   id: number;
+   userId: IUser;
+   categoryId: ICategory;
+   amount: number;
+   description: string;
+   month: Date;
+   createdAt: Date;
+   updatedAt?: Date;
+}
