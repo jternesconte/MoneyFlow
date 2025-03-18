547a2951a87bfdb3bc74ae4df96dd9bc3ab84f86
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-18 07:50:58 -0300

    feat: adds expense interface

---
+++ back/src/interfaces/Expense.ts
@@ -0,0 +1,13 @@
+import { ICategory } from "./ICategory";
+import { IUser } from "./IUser";
+
+export interface IExpense {
+   id: number;
+   userId: IUser;
+   categoryId: ICategory;
+   amount: number;
+   description: string;
+   date: Date;
+   createdAt: Date;
+   updatedAt?: Date;
+}
