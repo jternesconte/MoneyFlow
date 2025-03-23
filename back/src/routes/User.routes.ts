a78a4b2ecd59aa0f78bac469960cfcc90aa243ca
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-23 18:53:19 -0300

    feat: implemens the user routes

--- back/src/routes/User.routes.ts
+++ back/src/routes/User.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/AuthenticateToken";
+import { UserController } from "../controllers/User.controller";
 
 export const userRoutes = Router();
 
-/* userRoutes.post('/register', new UserController().newRegister);
+userRoutes.post('/register', new UserController().newRegister);
 
 userRoutes.post('/login', new UserController().userLogin);
 
-userRoutes.put('/editUser', authenticateToken, new UserController().editUser); */
+userRoutes.put('/editUser', authenticateToken, new UserController().editUser);
