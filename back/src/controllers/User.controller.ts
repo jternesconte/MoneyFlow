aba6e7374cdbd677f933946f30e445b8e885a479
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-24 06:47:01 -0300

    feat: implements User controller methods

--- back/src/controllers/User.controller.ts
+++ back/src/controllers/User.controller.ts
@@ -1,6 +1,21 @@
+import { Request, Response } from 'express';
+import { UserService } from '../services/User.service';
+
 export class UserController {
 
-   async newRegister(req: Request, res: Response) {
-       
+   private userService = new UserService();
+
+   async newRegister(req: Request, res: Response): Promise<void> {
+      const { name, email, password, password2 } = req.body;
+  
+      const result = await this.userService.registerUser(name, email, password, password2);
+      res.status(result.success ? 201 : 400).json(result);
+    }
+
+   async userLogin(req: Request, res: Response) {
+      const { email, password } = req.body;
+
+      const result = await this.userService.userLogin(email, password);
+      res.status(result.success ? 201 : 400).json(result);
    }
 }
