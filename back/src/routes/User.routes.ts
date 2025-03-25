fcfc1554b6cb79c770e00c6d9248cbdbcacac069
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-25 06:50:56 -0300

    fix: removes the editUser route

--- back/src/routes/User.routes.ts
+++ back/src/routes/User.routes.ts
@@ -7,5 +7,3 @@ export const userRoutes = Router();
 userRoutes.post('/register', new UserController().newRegister);
 
 userRoutes.post('/login', new UserController().userLogin);
-
-userRoutes.put('/editUser', authenticateToken, new UserController().editUser);
