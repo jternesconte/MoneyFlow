0dff6c1076500aff9034984068e6ff22a37e0782
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-26 06:44:11 -0300

    feat: adds newCategory route

--- back/src/routes/Category.routes.ts
+++ back/src/routes/Category.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/AuthenticateToken";
+import { CategoryController } from "../controllers/Category.controller";
 
 export const categoryRoutes = Router();
 
 categoryRoutes.use(authenticateToken);
+
+categoryRoutes.post('/newCategory', new CategoryController().newCategory)
