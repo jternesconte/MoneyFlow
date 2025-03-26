c49cd01cfefcbf051e264a23b37f62f8e1ea2a34
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-26 06:43:47 -0300

    feat: adds newCategory controller

--- back/src/controllers/Category.controller.ts
+++ back/src/controllers/Category.controller.ts
@@ -1,3 +1,20 @@
+import { Request, Response } from 'express';
+import { ICategory } from '../interfaces/ICategory';
+import { CategoryService } from '../services/Category.service';
+
 export class CategoryController {
+
+   private categoryService = new CategoryService();
+
+   async  newCategory(req: Request, res: Response) {
+      const { name, description } = req.body;
+  
+      const newCategory: ICategory | null = await this.categoryService.newCategory(name, description);
+      if(newCategory) {
+         res.status(200).json(newCategory);
+      } else {
+         res.status(400).json({ msg: 'Error in Category creation.' })
+      }
+   }
 
 }
