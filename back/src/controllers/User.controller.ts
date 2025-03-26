7b6771cb74a99614adf5c5b9095d0f91dfe9583b
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-26 20:43:33 -0300

    refactor: changes the way of call the controller

--- back/src/controllers/User.controller.ts
+++ back/src/controllers/User.controller.ts
@@ -4,15 +4,15 @@ import { UserService } from '../services/User.service';
 export class UserController {
 
    private userService = new UserService();
-
-   async newRegister(req: Request, res: Response) {
+   
+   newRegister = async (req: Request, res: Response) => {
       const { name, email, password, password2 } = req.body;
   
       const result = await this.userService.registerUser(name, email, password, password2);
       res.status(result.success ? 201 : 400).json(result);
     }
 
-   async userLogin(req: Request, res: Response) {
+   userLogin = async (req: Request, res: Response) => {
       const { email, password } = req.body;
 
       const result = await this.userService.userLogin(email, password);
