a9d48c3cc830f3d265ee2c792fd7aad42a53a510
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-26 06:43:57 -0300

    feat: adds new category service

--- back/src/services/Category.service.ts
+++ back/src/services/Category.service.ts
@@ -1,3 +1,24 @@
+import { format } from "date-fns";
+import { ICategory } from "../interfaces/ICategory";
+import { categoryRepository } from "../repositories/CategoryRepository";
+
 export class CategoryService {
 
+   async newCategory(name: string, description: string): Promise<ICategory | null> {
+      let newCategory: ICategory;
+
+      if(!name || !description) {
+         return null;
+      }
+
+      newCategory = {
+         name: name,
+         description:description,
+         createdAt: new Date()
+      }
+
+      categoryRepository.saveCategory(newCategory);
+
+      return newCategory;
+   }
 }
