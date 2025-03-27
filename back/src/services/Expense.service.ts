d533020be8d51e1f024d56816c25ed0dd6b5a6ff
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-27 06:52:07 -0300

    feat: adds newExpense service

--- back/src/services/Expense.service.ts
+++ back/src/services/Expense.service.ts
@@ -1,3 +1,40 @@
+import { IExpense } from './../interfaces/IExpense';
+import { userRepository } from "../repositories/UserRepository";
+import { IUser } from '../interfaces/IUser';
+import { categoryRepository } from '../repositories/CategoryRepository';
+import { ICategory } from '../interfaces/ICategory';
+import { expenseRepository } from '../repositories/ExpenseRepository';
+
 export class ExpenseService {
 
+   async newExpense(categoryId: number, amount: number, description: string, date: string, userId: number): Promise<IExpense | { msg: string; code: number }> {
+      let newExpense: IExpense;
+
+      if(!categoryId || !amount|| !description || !date) {
+         return { msg: "Not received some expense information", code: 400 };
+      }
+
+      let loggedUser: IUser = await userRepository.findUserById(userId);
+      if(!loggedUser) {
+         return { msg: "User not found", code: 404 };
+      }
+
+      let category: ICategory = await categoryRepository.findCategoryById(categoryId);
+      if(!category) {
+         return { msg: "Category not found", code: 404 };
+      }
+
+      newExpense = {
+         user: loggedUser,
+         category,
+         amount,
+         description,
+         date: new Date(date),
+         createdAt: new Date()
+      }
+
+      expenseRepository.saveExpense(newExpense);
+
+      return newExpense;
+   }
 }
