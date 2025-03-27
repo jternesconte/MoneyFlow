07d03b7775b40dc9827dbe616392df16380fee75
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-27 06:51:57 -0300

    feat: adds newExpense rotue

--- back/src/routes/Expense.routes.ts
+++ back/src/routes/Expense.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/AuthenticateToken";
+import { ExpenseController } from "../controllers/Expense.controller";
 
 export const expenseRoutes = Router();
 
 expenseRoutes.use(authenticateToken);
+
+expenseRoutes.post('/newExpense', new ExpenseController().newExpense)
