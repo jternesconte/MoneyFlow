93de06b6b3e15fd7a03f275525ffd35b65a38d8e
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-27 22:32:31 -0300

    feat: implements get Expenses by interval and category

--- back/src/repositories/ExpenseRepository.ts
+++ back/src/repositories/ExpenseRepository.ts
@@ -1,3 +1,4 @@
+import { Between } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Expense } from "../entities/Expense";
 import { IExpense } from "../interfaces/IExpense";
@@ -6,8 +7,8 @@ export const expenseRepository = AppDataSource.getRepository(Expense).extend({
 
  async saveExpense(data: IExpense): Promise<Expense> {
    const expense = this.create({
-      userId: data.user,
-      categoryId: data.category,
+      user: data.user,
+      category: data.category,
       amount: data.amount,
       description: data.description,
       date: data.date,
@@ -16,6 +17,32 @@ export const expenseRepository = AppDataSource.getRepository(Expense).extend({
 
    return this.save(expense);
  },
+
+ async findAllByInterval(days: number): Promise<IExpense[]> {
+   const startDate = new Date();
+  const finalDate = new Date();
+  startDate.setDate(finalDate.getDate() - days);
+  
+    const expenses = await this.find({
+      where: {
+        date: Between(startDate, finalDate)
+      },
+      relations: ["category", "user"],
+    });
+
+    return expenses as IExpense[];
+  },
+
+  async findAllByCategory(categoryId: number): Promise<IExpense[]> {
+     const expenses = await this.find({
+        where: {
+          category: { id: categoryId }
+        },
+        relations: ["category", "user"],
+     });
+ 
+     return expenses as IExpense[];
+   },
 
  
 });
