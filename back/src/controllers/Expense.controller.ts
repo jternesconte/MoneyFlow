fcb493fa88600eeefc5b84b0d27aa8c0f4897b1d
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-31 07:49:19 -0300

    refactor: changes order of saveExpense

--- back/src/controllers/Expense.controller.ts
+++ back/src/controllers/Expense.controller.ts
@@ -22,8 +22,8 @@ export class ExpenseController {
       if("code" in newExpense) {
          res.status(newExpense.code).json({ msg: newExpense.msg })
       } else {
+         await expenseRepository.saveExpense(newExpense);
          res.status(200).json(newExpense);
-         expenseRepository.saveExpense(newExpense);
       }
    }
 
